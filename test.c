a2d1f1e17972c093dd334f909dd76eb0ad032134
zoey-yjj <zoeyyyjj@gmail.com>
2023-02-25 09:12:47 +0800

    add test for char device

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define DEVICE_FILE "/dev/onebyte"
+
+int main(int argc, char *argv[]) {}
