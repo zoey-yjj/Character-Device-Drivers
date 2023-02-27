8d35239c002661043eb82e36690539bad21ce292
zoey-yjj <zoeyyyjj@gmail.com>
2023-02-27 16:06:32 +0800

    read and varify written value

--- test.c
+++ test.c
@@ -39,8 +39,26 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    // Read a byte from the device
+    memset(buffer, 0, sizeof(buffer));
+    ret = read(fd, buffer, 1);
+    if (ret == -1) {
+        fprintf(stderr, "Failed to read from %s: %s\n", DEVICE_FILE, strerror(errno));
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Verify that the read byte is the same as the written byte
+    if (buffer[0] != 'A') {
+        fprintf(stderr, "Expected 'A', got '%c'\n", buffer[0]);
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
     // Close the device file
     close(fd);
 
     printf("Success!\n");
+
+    return 0;
 }
